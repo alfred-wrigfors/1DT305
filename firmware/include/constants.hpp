a76d2b15cafb0313c06a7c8c462059f14a9600a4
Alfred Wrigfors <alfred.wrigfors@gmail.com>
2025-07-07 23:39:54 +0200

    Constants

--- firmware/include/constants.hpp
+++ firmware/include/constants.hpp
@@ -2,7 +2,21 @@
 #define CONSTANTS_HPP
 
 // WiFi
-const char* AP_SSID     = "AirWarden";
-const char* AP_PASSWORD = "12345678";
+const char* SSID     = "QWERTY";
+const char* PASSWORD = "QWERTY";
+
+const char* SERVER      = "192.168.1.28";
+const int   PORT        = 5000;
+const char* ENDPOINT    = "api/put";
+
+const float TEMP_COEFF  = -0.3158845;
+const float TEMP_OFFSET =  590.3520278;
+
+const int   RETRIES     = 5;
+
+const float BAT_COEFF   = 3.18;
+const float BAT_MIN     = 7.2;
+const float BAT_MAX     = 9.6;
+
 
 #endif
