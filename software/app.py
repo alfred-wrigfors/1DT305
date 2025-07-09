6420928e22646f635c94232f02236b858cee222a
Alfred Wrigfors <alfred.wrigfors@gmail.com>
2025-07-10 00:17:16 +0200

    Refactor to make the code easier to read

--- software/app.py
+++ software/app.py
@@ -11,35 +11,10 @@ def split_list(lst, n):
     k, m = divmod(len(lst), n)
     return [lst[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n)]
 
-
-# def moving_average(data, key, window_size, new_key = "value2"):
-#     if not data or window_size <= 0:
-#         return data  # handle empty input or invalid window
-
-#     window = deque()
-#     window_sum = 0.0
-
-#     for i, item in enumerate(data):
-#         value = item.get(key, 0)
-#         window.append(value)
-#         window_sum += value
-
-#         # Ensure the window is of the correct size
-#         if len(window) > window_size:
-#             window_sum -= window.popleft()
-
-#         avg = window_sum / len(window)
-#         item[new_key] = avg
-
-#     return data
-
 @app.route("/")
 def hello():
     return render_template('index.html')
 
-@app.route("/api/get")
-def get():
-    return database.put_water(34.4)
 
 @app.route("/api/put/<channel>")
 def put(channel):
@@ -71,81 +46,42 @@ def put_all():
     success = False
     t = time.time()
 
-    try:
-        data = float(request.args.get('water'))
-        if data is not None:
-            database.put_water(data, t)
-        success = True
-    except Exception:
-        pass
+    keyword_map = {
+        "water"     : database.put_water,
+        "air"       : database.put_air,
+        "humid"     : database.put_humid,
+        "voltage"   : database.put_voltage,
+        "soc"       : database.put_soc,
+    }
 
-    try:
-        data = float(request.args.get('air'))
-        if data is not None:
-            database.put_air(data, t)
-        success = True
-    except Exception:
-        pass
-
-    try:
-        data = float(request.args.get('humid'))
-        if data is not None:
-            database.put_humid(data, t)
-        success = True
-    except Exception:
-        pass
-
-    try:
-        data = float(request.args.get('voltage'))
-        if data is not None:
-            database.put_voltage(data, t)
-        success = True
-    except Exception:
-        pass
-
-    try:
-        data = float(request.args.get('soc'))
-        if data is not None:
-            database.put_soc(data, t)
-        success = True
-    except Exception:
-        pass
+    for keyword in keyword_map.keys():
+        try:
+            data = float(request.args.get(keyword, t))
+            if data is not None:
+                keyword_map[keyword](data)
+            success = True
+        except Exception:
+            pass
 
     return str(success)
 
-
+def handle_data(list_param: list, n: int) -> list[dict]:
+    data   = [item for item in list_param if item['value'] < 100]
+    data   = sorted(data, key=lambda x: x['time'])
+    data   = [{'time': sum([k['time'] for k in x]) / len(x), 'value': sum([k['value'] for k in x]) / len(x)} for x in split_list(data, n)]
+    return data
 
 @app.route("/api/fetch/<t>")
 def fetch(t):
     n = 500
-    bin = 1
     try:
         t = time.time() - float(t)
 
-        water   = database.get_water(t)
-        water   = sorted(water, key=lambda x: x['time'])
-        # water   = moving_average(water, key='value', window_size=bin)
-        water   = [{'time': sum([k['time'] for k in x]) / len(x), 'value': sum([k['value'] for k in x]) / len(x)} for x in split_list(water, n)]
-
-        air   = database.get_air(t)
-        air   = sorted(air, key=lambda x: x['time'])
-        # air   = moving_average(air, key='value', window_size=bin)
-        air   = [{'time': sum([k['time'] for k in x]) / len(x), 'value': sum([k['value'] for k in x]) / len(x)} for x in split_list(air, n)]
-
-        humid   = database.get_humid(t)
-        humid   = sorted(humid, key=lambda x: x['time'])
-        humid   = moving_average(humid, key='value', window_size=bin)
-        humid   = [{'time': sum([k['time'] for k in x]) / len(x), 'value': sum([k['value'] for k in x]) / len(x)} for x in split_list(humid, n)]
-
-        voltage   = database.get_voltage(t)
-        voltage   = sorted(voltage, key=lambda x: x['time'])
-        # voltage   = moving_average(voltage, key='value', window_size=bin)
-        voltage   = [{'time': sum([k['time'] for k in x]) / len(x), 'value': sum([k['value'] for k in x]) / len(x)} for x in split_list(voltage, n)]
-
-        soc   = database.get_soc(t)
-        soc   = sorted(soc, key=lambda x: x['time'])
-        # soc   = moving_average(soc, key='value', window_size=bin)
-        soc   = [{'time': sum([k['time'] for k in x]) / len(x), 'value': sum([k['value'] for k in x]) / len(x)} for x in split_list(soc, n)]
+        water   = handle_data(database.get_water(t), n)
+        air     = handle_data(database.get_air(t), n)
+        humid   = handle_data(database.get_humid(t), n)
+        voltage = handle_data(database.get_voltage(t), n)
+        soc     = handle_data(database.get_soc(t), n)
 
         return {'water': water, 'air': air, 'humid': humid, 'voltage': voltage, 'soc': soc}
     except Exception as e:
