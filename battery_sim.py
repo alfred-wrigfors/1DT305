1197d13727889725a69794baeebf5b0f85d953cb
Alfred Wrigfors <alfred.wrigfors@gmail.com>
2025-07-27 23:33:52 +0200

    Added more accurate value to simulation

--- battery_sim.py
+++ battery_sim.py
@@ -7,7 +7,7 @@ class Sun:
         self.slope  = slope
         
     def update(self, time: float):
-        time = time % 24.0
+        time = (time / (60 * 60)) % 24.0
         if time <= self.start or time >= self.stop:
             return 0
         
@@ -24,38 +24,37 @@ class Battery:
         self.energy -= power * delta
         self.energy = min(max(self.energy, 0), self.max_energy)
         return self.energy / self.max_energy
-        
 
-POWER_DRAW              = {
-    "ESP32":    3.3 * 0.140,
-    "TARVOS":   3.3 * 0.023,
-    "DIODE":    0.7 * 0.200,
-    "DC-DC":    0.2,
-    "CHARGER":  0.1
-}
+class Device:
+    def __init__(self, draw: float, idle: float, time: float | int, duty: float | int):
+        self.draw = draw
+        self.idle = idle
+        self.time = time
+        self.duty = duty
+    
+    def update(self, time: float):
+        return self.draw if time % self.duty <= self.time else self.idle
 
-POWER_DRAW              = sum(POWER_DRAW.values())
-
-POWER_DRAW_DURATION     = 0.5
-POWER_DRAW_PERIOD       = 10.0
 
 SOLAR_POWER             = 0.5
-SOLAR_EFFICIENCY_FACTOR = 0.38
+SOLAR_EFFICIENCY_FACTOR = 0.4
 
-sun = Sun(9.0, 17.5, 1/2)
-bat = Battery(3.0*3600, 3.7, 1.0)
+sun = Sun(11.0, 17.5, 1/2)
+battery = Battery(3.0*3600, 3.7, 1.0)
+device = Device(0.25*3.3, 0.01, 2.5, 30)
 
 soc = []
 time = []
-for i in range(60*60*24*31 + 1):
-    solar   = sun.update(i / (60 * 60)) * SOLAR_POWER * SOLAR_EFFICIENCY_FACTOR
-    device  = POWER_DRAW * (i % int(POWER_DRAW_PERIOD) in range(int(POWER_DRAW_DURATION)))
-    soc.append(bat.update(device - solar, 1))
-    time.append(i / (60.0*60.0*24))
+for i in range((60*60*24*31 + 1) * 10):
+    t = i / 10
+    solar   = sun.update(t) * SOLAR_POWER * SOLAR_EFFICIENCY_FACTOR
+    esp     = device.update(t)
+    soc.append(battery.update(esp - solar, 0.1))
+    time.append(i / (60*60))
     if soc[-1] <= 0:
         break
 
-print(str(int(i / (60.0 * 60.0 * 24.0))) + " days and " + str(int((i / (60.0 * 60.0)) % 24)) + " hours")
+print(str(int(i / (60.0 * 60.0 * 24.0 * 10))) + " days and " + str(int((i / (60.0 * 60.0 * 10)) % 24)) + " hours")
 
 plt.plot(time, soc)
 plt.ylim(-0.1, 1.1)
